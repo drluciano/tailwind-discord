3627084056f0d0d764983871f203ac4b3e6d8087
drluciano <dylan.jlr@gmail.com>
2023-05-15 19:49:27 -0500

    rough.

---
+++ src/app/Toolbar.js
@@ -0,0 +1,10 @@
+import { FaFire, FaPoo, FaDiceD20 } from "react-icons/fa";
+import { BsPlus, BsFillLightningFill } from "react-icons/bs";
+
+const Toolbar = () => {
+    return(
+        <div className="fixed h-12 w-60 bg-gray-800 rounded-lg shadow-md items-center justify-center"></div>
+    );
+}
+
+export default Toolbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import SideBar from "./SideBar";
+import Toolbar from "./Toolbar";
 
 export default function Home() {
-  return <div className="flex">
+  return <div className="flex w-screen h-screen">
     <SideBar />
+    <Toolbar />
   </div>;
 }
